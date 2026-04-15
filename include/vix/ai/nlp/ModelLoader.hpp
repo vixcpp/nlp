b96abb914024ccee7c607f451f94aefe0e76ec3f
Gaspard Kirira <gaspardkirira9@gmail.com>
2026-04-15 16:45:15 +0300

    refactor(nlp): align module with Vix conventions and clean legacy files
    - normalize CMake (namespace, install, package config)
    - update NLP headers (tokenizer, embedding, pipeline, model loader)
    - update tests to use vix::ai::nlp
    - remove legacy Makefile and scripts
    - add cmake/ package configuration
    - update .gitignore

--- include/vix/ai/nlp/ModelLoader.hpp
+++ include/vix/ai/nlp/ModelLoader.hpp
@@ -1,12 +1,16 @@
 /**
- * @file ModelLoader.hpp
- * @author Gaspard Kirira
  *
- * Copyright 2025, Gaspard Kirira. All rights reserved.
- * https://github.com/vixcpp/vix
- * Use of this source code is governed by a MIT license that can be found in the License file.
+ *  @file ModelLoader.hpp
+ *  @author Gaspard Kirira
  *
- * Vix.cpp
+ *  Copyright 2025, Gaspard Kirira.
+ *  All rights reserved.
+ *  https://github.com/vixcpp/vix
+ *
+ *  Use of this source code is governed by a MIT license
+ *  that can be found in the License file.
+ *
+ *  Vix.cpp
  */
 #ifndef VIX_AI_NLP_MODEL_LOADER_HPP
 #define VIX_AI_NLP_MODEL_LOADER_HPP
